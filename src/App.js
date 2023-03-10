c18e12bcc3b92929afd6f205f462d6dcfcf75e79
KarlPreisler <Karlpreisler@gmail.com>
2023-03-10 12:59:07 +0000

    Added navbar component, logo and favicons

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
-import logo from './logo.svg';
-import './App.css';
+import Button from "react-bootstrap/Button";
+import "./App.css";
+import NavBar from "./components/NavBar";
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello world!</h1>
+      <NavBar />
     </div>
   );
 }
