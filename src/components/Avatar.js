5811a639a554ea4ce2455c2477d30d69b32da27c
KarlPreisler <Karlpreisler@gmail.com>
2023-03-11 16:47:02 +0000

    Added page to create recipe, needs correct field input

--- src/components/Avatar.js
+++ src/components/Avatar.js
@@ -1,12 +1,12 @@
 import React from "react";
 import styles from "../styles/Avatar.module.css";
 
-const Avatar = ({ src, height = 45, text }) => {
+const Avatar = ({ height = 45, text }) => {
   return (
     <span>
       <img
         className={styles.Avatar}
-        src={src}
+        src={"https://res.cloudinary.com/dlorcehfq/image/upload/v1678551729/default_profile_y7itwz.jpg"}
         height={height}
         width={height}
         alt="avatar"
