5807b03b2a4e89f1dd1f3db029d5d15d345726c4
KarlPreisler <Karlpreisler@gmail.com>
2023-03-13 16:57:57 +0000

    Added comments folder, Comment file and css. full functionality for adding comment

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ import RecipeCreateForm from "./pages/recipes/RecipeCreateForm";
 import RecipePage from "./pages/recipes/RecipePage";
 import RecipesPage from "./pages/recipes/RecipesPage";
 import { useCurrentUser } from "./contexts/CurrentUserContext";
+import RecipeEditForm from "./pages/recipes/RecipeEditForm";
 
 function App() {
   const currentUser = useCurrentUser();
@@ -50,6 +51,7 @@ function App() {
           <Route exact path="/signup" render={() => <SignUpForm />} />
           <Route exact path="/recipes/create" render={() => <RecipeCreateForm />} />
           <Route exact path="/recipes/:id" render={() => <RecipePage />} />
+          <Route exact path="/recipes/:id/edit" render={() => <RecipeEditForm />} />
           <Route render={() => <p>Page not found!</p>} />
         </Switch>
       </Container>
--- src/pages/recipes/RecipePage.js
+++ src/pages/recipes/RecipePage.js
@@ -8,19 +8,28 @@ import appStyles from "../../App.module.css";
 import { useParams } from "react-router";
 import { axiosReq } from "../../api/axiosDefaults";
 import Recipe from "./Recipe";
+import Comment from "../comments/Comment";
+
+import CommentCreateForm from "../comments/CommentCreateForm";
+import { useCurrentUser } from "../../contexts/CurrentUserContext";
 
 function RecipePage() {
   const { id } = useParams();
   const [recipe, setRecipe] = useState({ results: [] });
 
+  const currentUser = useCurrentUser();
+  const profile_image = currentUser?.profile_image;
+  const [comments, setComments] = useState({ results: [] });
+
   useEffect(() => {
     const handleMount = async () => {
       try {
-        const [{ data: recipe }] = await Promise.all([
+        const [{ data: recipe }, { data: comments }] = await Promise.all([
           axiosReq.get(`/recipes/${id}`),
+          axiosReq.get(`/comments/?recipe=${id}`),
         ]);
         setRecipe({ results: [recipe] });
-        console.log(recipe);
+        setComments(comments);
       } catch (err) {
         console.log(err);
       }
@@ -34,7 +43,28 @@ function RecipePage() {
       <Col className="py-2 p-0 p-lg-2" lg={8}>
         <p>Popular profiles for mobile</p>
         <Recipe {...recipe.results[0]} setRecipes={setRecipe} recipePage/>
-        <Container className={appStyles.Content}>Comments</Container>
+        <Container className={appStyles.Content}>
+          {currentUser ? (
+            <CommentCreateForm
+              profile_id={currentUser.profile_id}
+              profileImage={profile_image}
+              recipe={id}
+              setRecipe={setRecipe}
+              setComments={setComments}
+            />
+          ) : comments.results.length ? (
+            "Comments"
+          ) : null}
+          {comments.results.length ? (
+            comments.results.map((comment) => (
+              <Comment key={comment.id} {...comment} />
+            ))
+          ) : currentUser ? (
+            <span>No comments yet, be the first to comment!</span>
+          ) : (
+            <span>No comments... yet</span>
+          )}
+        </Container>
       </Col>
       <Col lg={4} className="d-none d-lg-block p-0 p-lg-2">
         Popular profiles for desktop
