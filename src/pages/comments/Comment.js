ac8177e163caebcdd6d67206f71eab11e6174ff1
KarlPreisler <Karlpreisler@gmail.com>
2023-03-14 19:36:37 +0000

    Added edit functionality for user comments

--- src/pages/comments/Comment.js
+++ src/pages/comments/Comment.js
@@ -1,8 +1,10 @@
-import React from "react";
+import React, { useState } from "react";
 import { Media } from "react-bootstrap";
 import { Link } from "react-router-dom";
 import Avatar from "../../components/Avatar";
 import { MoreDropdown } from "../../components/MoreDropdown";
+import CommentEditForm from "./CommentEditForm";
+
 import styles from "../../styles/Comment.module.css";
 import { useCurrentUser } from "../../contexts/CurrentUserContext";
 import { axiosRes } from "../../api/axiosDefaults";
@@ -21,6 +23,7 @@ const Comment = (props) => {
     setComments, 
   } = props;
 
+  const [showEditForm, setShowEditForm] = useState(false);
   const currentUser = useCurrentUser();
   const is_owner = currentUser?.username === owner;
 
@@ -56,7 +59,7 @@ const Comment = (props) => {
   }
 
   return (
-    <div>
+    <>
       <hr />
       <Media>
         <Link to={`/profiles/${profile_id}`}>
@@ -65,16 +68,31 @@ const Comment = (props) => {
         <Media.Body className="align-self-center ml-2">
           <span className={styles.Owner}>{owner}</span>
           <span className={styles.Date}>{updated_at}</span>
+          {showEditForm ? (
+            <CommentEditForm
+              id={id}
+              profile_id={profile_id}
+              content={content}
+              rating={rating}
+              profileImage={profile_image}
+              setComments={setComments}
+              setShowEditForm={setShowEditForm}
+            />
+          ) : (
+          <>
           <p>{content}</p>
-          <p>
-            {stars} {rating}/5
-          </p>
+          <p>{stars} {rating}/5</p>
+          </>
+          )}
         </Media.Body>
-        {is_owner && (
-          <MoreDropdown handleEdit={() => {}} handleDelete={handleDelete} />
+        {is_owner && !showEditForm && (
+          <MoreDropdown
+            handleEdit={() => setShowEditForm(true)}
+            handleDelete={handleDelete}
+          />
         )}
       </Media>
-    </div>
+    </>
   );
 };
 
---
+++ src/pages/comments/CommentEditForm.js
@@ -0,0 +1,93 @@
+import React, { useState } from "react";
+
+import Form from "react-bootstrap/Form";
+import { axiosRes } from "../../api/axiosDefaults";
+
+import styles from "../../styles/CommentCreateEditForm.module.css";
+
+function CommentEditForm(props) {
+  const { id, content, rating, setShowEditForm, setComments } = props;
+
+  const [formContent, setFormContent] = useState(content);
+  const [formRating, setFormRating] = useState(rating);
+
+  const handleContentChange = (event) => {
+    setFormContent(event.target.value);
+  };
+
+  const handleRatingChange = (event) => {
+    setFormRating(event.target.value);
+  };
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    try {
+      await axiosRes.put(`/comments/${id}/`, {
+        content: formContent.trim(),
+        rating: formRating.trim(),
+      });
+      setComments((prevComments) => ({
+        ...prevComments,
+        results: prevComments.results.map((comment) => {
+          return comment.id === id
+            ? {
+                ...comment,
+                content: formContent.trim(),
+                rating: formRating.trim(),
+                updated_at: "now",
+              }
+            : comment;
+        }),
+      }));
+      setShowEditForm(false);
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  return (
+    <Form onSubmit={handleSubmit}>
+      <Form.Group className="pr-1">
+        <Form.Control
+          className={styles.Form}
+          as="textarea"
+          value={formContent}
+          onChange={handleContentChange}
+          rows={2}
+        />
+        <Form.Control
+          as="select"
+          className={styles.Form}
+          name="rating"
+          value={formRating}
+          onChange={handleRatingChange}
+        >
+          <option value="">Select rating</option>
+          <option value="1">1 star</option>
+          <option value="2">2 stars</option>
+          <option value="3">3 stars</option>
+          <option value="4">4 stars</option>
+          <option value="5">5 stars</option>
+        </Form.Control>
+      </Form.Group>
+      <div className="text-right">
+        <button
+          className={styles.Button}
+          onClick={() => setShowEditForm(false)}
+          type="button"
+        >
+          cancel
+        </button>
+        <button
+          className={styles.Button}
+          disabled={!content.trim()}
+          type="submit"
+        >
+          save
+        </button>
+      </div>
+    </Form>
+  );
+}
+
+export default CommentEditForm;
