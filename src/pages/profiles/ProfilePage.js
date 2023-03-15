8c08e56a3fd97011eed802ed9fb3e6f53a057340
KarlPreisler <Karlpreisler@gmail.com>
2023-03-15 15:10:14 +0000

    Added edit profile functionality and mount to update image correctly

--- src/pages/profiles/ProfilePage.js
+++ src/pages/profiles/ProfilePage.js
@@ -23,6 +23,7 @@ import InfiniteScroll from "react-infinite-scroll-component";
 import Recipe from "../recipes/Recipe";
 import { fetchMoreData } from "../../utils/utils";
 import NoResults from "../../assets/no-results.png";
+import { ProfileEditDropdown } from "../../components/MoreDropdown";
 
 function ProfilePage() {
   const [hasLoaded, setHasLoaded] = useState(false);
@@ -60,12 +61,13 @@ function ProfilePage() {
 
   const mainProfile = (
     <>
+    {profile?.is_owner && <ProfileEditDropdown id={profile?.id} />}
       <Row noGutters className="px-3 text-center">
         <Col lg={3} className="text-lg-left">
           <Image
             className={styles.ProfileImage}
             roundedCircle
-            src="https://res.cloudinary.com/dlorcehfq/image/upload/v1678551729/default_profile_y7itwz.jpg"
+            src={profile?.image}
           />
         </Col>
         <Col lg={6}>
